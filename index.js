d66cdd60e8849270c901c4c175d7e5180e545ca8
Devyanshu Negi <devyanshunegi@gmail.com>
2024-03-04 18:34:25 +0530

    this is improved with some http routes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pg from "pg";
+// import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+/*
 const db = new pg.Client({
 	user: "postgres",
 	host: "localhost",
@@ -11,8 +13,22 @@ const db = new pg.Client({
 	password: "asdfjkl;",
 	port: 5432,
 });
+
 db.connect();
+*/
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+    res.send("Home Page")
+});
+
+app.get("/login", (req, res) => {
+    res.render("search.ejs");
+})
+
+app.listen(port, () => {
+	console.log(`Server running on http://localhost:${port}`);
+});
