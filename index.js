1abb0de91438043840ab1a69234167606170032c
Devyanshu Negi <devyanshunegi@gmail.com>
2024-03-07 22:25:25 +0530

    google added but not working and db pending

--- index.js
+++ index.js
@@ -1,55 +1,206 @@
+/** @format */
+
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-import { name } from "ejs";
-
-// for dirname to send html file
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-import fs from "fs";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
+env.config();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-const db = new pg.Client({
-	user: "postgres",
-	host: "localhost",
-	database: "hack",
-	password: "asdfjkl;",
-	port: 5432,
-});
-
-db.connect();
-
-
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: true,
+	})
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => { // done home page
-	res.sendFile(__dirname + "/views"+"/home.html");
+app.use(passport.initialize());
+app.use(passport.session());
+
+const db = new pg.Client({
+	user: process.env.PG_USER,
+	host: process.env.PG_HOST,
+	database: process.env.PG_DATABASE,
+	password: process.env.PG_PASSWORD,
+	port: process.env.PG_PORT,
+});
+db.connect();
+
+app.get("/", (req, res) => {
+	res.render("home.ejs");
 });
 
-app.get("/login", (req, res) => { // login page
-    res.render("login.ejs")
-})
-app.get("/register", (req, res) => {
-    res.render("register.ejs")
-})
-
-app.post("/submit", (req, res) => {
-	var body = req.body;
-	console.log(body);
+app.get("/login", (req, res) => {
 	res.render("login.ejs");
-	
 });
 
+app.get("/register", (req, res) => {
+	res.render("register.ejs");
+});
+
+app.get("/logout", (req, res) => {
+	req.logout(function (err) {
+		if (err) {
+			return next(err);
+		}
+		res.redirect("/");
+	});
+});
+
+app.post("/submit", (req, res) =>{
+	
+})
+
+app.get("/dash", (req, res) => {
+	console.log(req);
+	if (req.isAuthenticated()) {
+		// here checking
+		res.render("dash.ejs");
+	} else {
+		res.redirect("/login");
+	}
+});
+
+app.get(
+	"/auth/google",
+	passport.authenticate("google", {
+		// * authentication
+		scope: ["profile", "email"],
+	})
+);
+
+app.get(
+	"/auth/google/dash",
+	passport.authenticate("google", {
+		// * the page after the login
+		successRedirect: "/dash",
+		failureRedirect: "/login",
+	})
+);
+
+app.post(
+	"/login",
+	passport.authenticate("local", {
+		// *** must use on every login
+		successRedirect: "/dash",
+		failureRedirect: "/login",
+	})
+);
+
+app.post("/register", async (req, res) => {
+	const email = req.body.mail;
+	const password = req.body.password;
+	console.log("the mail is ", email , " and pass is ", password);
+
+	try {
+		const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+			email,
+		]);
+
+		if (checkResult.rows.length > 0) {
+			res.redirect("/login");
+		} else {
+			bcrypt.hash(password, saltRounds, async (err, hash) => {
+				if (err) {
+					console.error("Error hashing password:", err);
+				} else {
+					const result = await db.query(
+						"INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
+						[email, hash]
+					);
+					const user = result.rows[0];
+					req.login(user, (err) => {
+						console.log("success");
+						res.redirect("/dash");
+					});
+				}
+			});
+		}
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+passport.use(
+	"local",
+	new Strategy(async function verify(username, password, cb) {
+		try {
+			const result = await db.query("SELECT * FROM users WHERE email = $1 ", [
+				username,
+			]);
+			if (result.rows.length > 0) {
+				const user = result.rows[0];
+				const storedHashedPassword = user.password;
+				bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+					if (err) {
+						console.error("Error comparing passwords:", err);
+						return cb(err);
+					} else {
+						if (valid) {
+							return cb(null, user);
+						} else {
+							return cb(null, false);
+						}
+					}
+				});
+			} else {
+				return cb("User not found");
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	})
+);
+
+passport.use(
+	"google",
+	new GoogleStrategy(
+		{
+			clientID: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			callbackURL: "http://localhost:3000/auth/google/dash",
+			userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+		},
+		async (accessToken, refreshToken, profile, cb) => {
+			try {
+				console.log(profile);
+				const result = await db.query("SELECT * FROM users WHERE email = $1", [
+					profile.email,
+				]);
+				if (result.rows.length === 0) {
+					const newUser = await db.query(
+						"INSERT INTO users (email, password) VALUES ($1, $2)",
+						[profile.email, "google"]
+					);
+					return cb(null, newUser.rows[0]);
+				} else {
+					return cb(null, result.rows[0]);
+				}
+			} catch (err) {
+				return cb(err);
+			}
+		}
+	)
+);
+passport.serializeUser((user, cb) => {
+	cb(null, user);
+});
+
+passport.deserializeUser((user, cb) => {
+	cb(null, user);
+});
 
 app.listen(port, () => {
-	console.log(`Server running on http://localhost:${port}`);
+	console.log(`Server running on port ${port}`);
 });
